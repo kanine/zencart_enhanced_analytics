3b47386455835b946517178ff0794fa9bf0ba140
kanine <keith.pickard@gmail.com>
2022-07-09 15:17:44 +1000

    tidy up code

--- includes/classes/observers/auto.enhanced_analytics.php
+++ includes/classes/observers/auto.enhanced_analytics.php
@@ -12,81 +12,19 @@
   private $_logDir = DIR_FS_CATALOG . 'logs/plugins/'; // Change this to preference
 
   function __construct() {
-      global $zco_notifier;
-      $this->attach($this, array('NOTIFY_HEADER_START_CHECKOUT_SUCCESS'));
+  
+    $this->attach($this, array('NOTIFY_HEADER_START_CHECKOUT_SUCCESS'));
+  
   }
 
-  function getID() {
-      $id = '';
-      if (isset($_REQUEST['products_id'])) {
-          if (is_array($_REQUEST['products_id'])) {  $id = explode(":", $_REQUEST['products_id'][0]);
-          } else {  $id = explode(":", $_REQUEST['products_id']);  }
-      } else {
-          if (isset($_REQUEST['product_id'])) {
-              if (is_array($_REQUEST['product_id'])) { $id = explode(":", $_REQUEST['product_id'][0]);
-              } else {  $id = explode(":", $_REQUEST['product_id']);  }
-          }
-      }
-      $id = (int)$id[0];
-      if ($id === 0) $id = "";
-  return $id;
-  }
-
-    
- function getCatString($id) {
-  global $db, $cPath ;
-  $masterCat = zen_get_categories_name_from_product($id) ;
-  $catTxt = '';
-     $i = 0 ; $flag = 0 ;
-        if(isset($cPath)) {
-            $p = explode('_',$cPath) ;
-            while ($i < count($p)) {
-                $the_categories_name= $db->Execute("select categories_name from " . TABLE_CATEGORIES_DESCRIPTION . " where categories_id= '" . $p[$i] . "' and language_id= '" . $_SESSION['languages_id'] . "'");
-                if ($masterCat ==  $the_categories_name->fields['categories_name'])  {$flag = 1 ;}
-                     
-            $i++ ;        
-            } 
-        $catTxt = substr($catTxt, 1);
-        }     
-  return ($flag != 1 ) ? $masterCat:$catTxt;      
-// return $catTxt ;    
- }   
-    
- function addProductItemsStr() { 
-    $itemsStr = "" ;  $i=0 ; 
-    $products = $_SESSION['cart']->get_products(); 
-    if(is_array($products)) { 
-      foreach ($products as $item) { 
-        if(is_array($item['attributes'])) $varTxt = zen_values_name($item['attributes'][1]);                     
-        if(!$varTxt) $varTxt = "n/a"; 
-        
-        $itemID = explode(":", $item['id'] ) ;
-
-        $brand = zen_get_products_manufacturers_name($itemID[0]) ; 
-        $brandTxt = ($brand != "") ? $brand:"n/a";
-        $itemsStr .= "ga('ec:addProduct',"
-                  . " {'id': '{$itemID[0]}',"
-                              . " 'name': '".addslashes($item['name'])."',"
-                              . " 'brand': '{$brandTxt}',"
-                              . " 'category': '". zen_get_categories_name_from_product($itemID[0])."' ,"
-                              . " 'variant': '{$varTxt}',"
-                              .  " 'price': '".number_format((float)($item['price'] + ($item['price'] *  $item['tax_class_id'] / 100 )) ,2,'.','')."',"
-                              . " 'quantity': '{$item['quantity']}',"
-                                . " 'position': '{$i}' } );\n";
-        $i++ ;  
-      }
-    }    
-    return $itemsStr ;           
-  }
-
-  function update(&$callingClass, $notifier, $paramsArray) {
+  public function update(&$callingClass, $notifier, $paramsArray) {
+  
     global $db, $analytics;
 
     switch ($notifier) {
       case 'NOTIFY_HEADER_START_CHECKOUT_SUCCESS': //  All Checkout complete/successful 
         $order_summary = $_SESSION['order_summary'];
         
-
         $coupon = isset($order_summary['coupon_code']) ? $order_summary['coupon_code'] : "n/a";
         
         $analytics['transaction'] = array('transaction_id' => (string)$order_summary['order_number'],
@@ -98,12 +36,10 @@
                                         );
         
         $items_query = "SELECT DISTINCT orders_products_id, products_id, products_name, products_model, final_price, products_tax, products_quantity
-            FROM " . TABLE_ORDERS_PRODUCTS . " WHERE orders_id = :ordersID ORDER BY products_name";
+                        FROM " . TABLE_ORDERS_PRODUCTS . " WHERE orders_id = :ordersID ORDER BY products_name";
 
         $items_query = $db->bindVars($items_query, ':ordersID', $order_summary['order_number'], 'integer');
         $items_in_cart = $db->Execute($items_query);
-
-        
         
         $i = 0 ; 
         
